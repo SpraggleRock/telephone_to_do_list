36ab45c4e84f2b3969204d02c67bbe04ab19cb23
& Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-04-01 11:08:26 -0500

    tasks redone

--- specs.rb
+++ specs.rb
@@ -1,17 +1,11 @@
 #here is where you require the file where the todo list is
 
 describe Task do
-  let(:task) { Task.new }
+  let(:task) { Task.new("lottery", "win the lottery", true) }
 
   context '#initialize' do
-    it "has a title" do
-      task.title.should be_a_kind_of String
-    end
-
-    it "has a description" do
-    end
-
-    it "has a status that is either complete or incomplete" do
+    it "creates a task object" do
+      task.should be_an_instance_of Task
     end
   end
 end
