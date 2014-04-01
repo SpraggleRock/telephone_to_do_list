23f7bc6dd221346563320ed32cd0e7f55323e395
& Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-04-01 11:20:55 -0500

    Name,description, status

--- specs.rb
+++ specs.rb
@@ -8,4 +8,24 @@ describe Task do
       task.should be_an_instance_of Task
     end
   end
+
+  context '#name' do
+    it "returns the task name" do
+      task.name.should eq "lottery"
+    end
+  end
+
+  context '#description' do
+    it 'returns the task description' do
+      task.description.should eq 'win the lottery'
+    end
+  end
+
+  context '#status' do
+    it 'returns the task status' do
+      task.description.should eq  true
+    end
+  end
+
+
 end
