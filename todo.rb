ee7e078b01f08246ee7273b61993829f93bade24
Kelsey & Adam Willis <pair+kma3a.whwebdev@devbootcamp.com>
2014-04-01 11:13:26 -0500

    implemented class Task with initial properties

--- todo.rb
+++ todo.rb
@@ -1,5 +1,9 @@
 class Task
-  def initialize
+  attr_reader :title, :description, :is_complete
 
+  def initialize(title,description,is_complete)
+    @title = title
+    @description = description
+    @is_complete = is_complete
   end
 end
